b5b38f47d797ddeb7476591e8e290f78da661575
Ole Kristian Lien <ole.k.lien@gmail.com>
2017-05-28 19:06:45 +0200

    added scripts to import csv files

---
+++ import-csv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#sudo apt install sqlite3
+
+sqlite3 quiz.db < import-csv.sql
---
+++ import-csv.sql
@@ -0,0 +1,4 @@
+.separator ,
+.import csv/users.csv users
+.import csv/quiz.csv quiz
+.import csv/questions.csv questions
